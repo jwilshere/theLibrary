67a482d3aed008bdb21be508b053e189fe0123b0
PhilipTheKung <philip_98_@hotmail.com>
2021-04-27 15:33:40 +0200

    f
    f

--- src/Admin.java
+++ src/Admin.java
@@ -1,3 +1,5 @@
+import java.sql.*;
+
 public class Admin {
     int id;
 
@@ -15,6 +17,39 @@ public class Admin {
     public void canUserBorrow(int id){
         //Jämföra id:t med databas idn för att se att den finns
         //Om den finns så kolla vilken type det är och vad borrowlimiten ligger på
+
+
+        try(Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://@vhost6.lnu.se:23306/1ik013v20-20?useSSL=false",
+                "1ik013v20-20", "kiwi")) {
+            System.out.println("Connected");
+
+            System.out.println();
+            System.out.println("---Album---");
+
+            Statement statement = conn.createStatement();
+
+            ResultSet result = statement.executeQuery("Select * FROM album");
+
+            while(result.next()){
+                System.out.println(result.getString(1) + ": " + result.getString(2) + ": " + result.getString(3));
+            }
+
+            System.out.println();
+            System.out.println("---Artister---");
+
+            Statement statement1 = conn.createStatement();
+
+            ResultSet result1 = statement1.executeQuery("Select * FROM artist");
+
+            while(result1.next()){
+                System.out.println(result1.getString(1) + ": " + result1.getString(2) + ": " + result1.getString(3));
+            }
+
+        }
+        catch(SQLException ex){
+            System.out.println("Something went wrong");
+        }
 
     }
 
