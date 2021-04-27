eda04a6ab20d71ed79b8ca41be289f97ddfa95a6
Hellgaard <83272454+Hellgaard@users.noreply.github.com>
2021-04-27 11:17:52 +0200

    w
    w

--- src/User.java
+++ src/User.java
@@ -1,2 +1,15 @@
 public class User {
+
+    int Id;
+    String Type;
+    int ItemBorrowed;
+    int BorrowLimit;
+    boolean Active;
+    int Delays;
+
+    public boolean canBorrow(){
+        return false;
+    }
+
+    public void borrowBook(){}
 }
