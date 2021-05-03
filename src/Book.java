5764973d24464135893bd80c92f4a0daab5bd51a
PhilipTheKung <philip_98_@hotmail.com>
2021-05-03 10:19:31 +0200

    k
    j

--- src/Book.java
+++ src/Book.java
@@ -8,5 +8,42 @@ public class Book {
     private int ISBN;
     private Date borrowed;
 
+    public Book(int id, String title, int ISBN, Date borrowed) {
+        this.id = id;
+        this.title = title;
+        this.ISBN = ISBN;
+        this.borrowed = borrowed;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public Date getBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(Date borrowed) {
+        this.borrowed = borrowed;
+    }
 }
--- src/Getset.java
+++ src/Getset.java
@@ -1,9 +1,12 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 public class Getset {
 
+    ArrayList<User> userLista = null;
+    ArrayList<Book> bookLista = null;
 
     public void connectDB(){
         try{
@@ -17,7 +20,7 @@ public class Getset {
 
     public User[] getUsers(){
         connectDB();
-        ArrayList<User> userLista = new ArrayList<>();
+        userLista = new ArrayList<>();
         int index = 0;
 
         try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Admin?serverTimezone=UTC",
@@ -30,17 +33,45 @@ public class Getset {
             while(rs.next()){
                 User anvandare = new User(rs.getInt("id"), rs.getString("Fnamn"), rs.getString("Lnamn"), rs.getInt("personid"), rs.getInt("type"), rs.getInt("itemBorrowed"), rs.getInt("borrowlimit"), rs.getInt("active"), rs.getInt("delays"));
                 userLista.add(anvandare);
+                index++;
             }
         }
         catch(SQLException ex){
             System.out.println("Something went wrong");
         }
-        User[] usLista = new User[10];
+        User[] usLista = new User[index];
         userLista.toArray(usLista);
         return usLista;
     }
 
-    public String getFnamn(int id){
+    public Book[] getBooks(){
+        connectDB();
+        bookLista = new ArrayList<>();
+        int index = 0;
+
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Admin?serverTimezone=UTC",
+                "root","philip98")) {
+            System.out.println("Connected");
+
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("Select * FROM book");
+
+            while(rs.next()){
+                Book bok = new Book(rs.getInt("id"), rs.getString("title"), rs.getInt("ISBN"), rs.getDate("borrowedOnDate"));
+                bookLista.add(bok);
+                index++;
+            }
+        }
+        catch(SQLException ex){
+            System.out.println("Something went wrong");
+        }
+        Book[] buLista = new Book[index];
+        bookLista.toArray(buLista);
+        return buLista;
+    }
+
+
+    /* public String getFnamn(int id){
         connectDB();
         String Fnamn = "";
 
@@ -61,6 +92,30 @@ public class Getset {
             System.out.println("Something went wrong");
         }
         return Fnamn;
+    } */
+
+    public void setUser(int id, String fnamn, String lnamn, int personId, int type, int itemBorrowed, int borrowLimit, int active, int delays){
+
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Admin?serverTimezone=UTC",
+                "root","philip98")) {
+            System.out.println("Connected");
+
+            PreparedStatement userIn = conn.prepareStatement("INSERT INTO User VALUES (?,?,?,?,?,?,?,?,?)");
+            userIn.setInt(1, id);
+            userIn.setString(2, fnamn);
+            userIn.setString(3, lnamn);
+            userIn.setInt(4, personId);
+            userIn.setInt(5, type);
+            userIn.setInt(6, itemBorrowed);
+            userIn.setInt(7, borrowLimit);
+            userIn.setInt(8, active);
+            userIn.setInt(9, delays);
+
+            userIn.executeUpdate();
+        }
+        catch (SQLException ex) {
+            System.out.println("Something went wrong" + ex.getMessage());
+        }
     }
 
     public static void main(String[] args) {
@@ -68,7 +123,21 @@ public class Getset {
 
         System.out.println(Arrays.toString(hej.getUsers()));
         System.out.println();
-        System.out.println(hej.getFnamn(1234));
+        System.out.println(Arrays.toString(hej.getBooks()));
+        System.out.println();
+
+        for(User s: hej.getUsers()){
+            System.out.println(s.getId());
+        }
+
+        System.out.println();
+
+        hej.setUser(5555, "Rasmus", "Johansson", 980428, 2, 2, 5, 1, 0);
+
+        for(User s: hej.getUsers()){
+            System.out.println(s.getId());
+        }
+
 
 
     }
