0c3fdb11feadad06dbf791e145fc730b1b897963
JoelJonson <joel.jonson@outlook.com>
2021-05-19 18:04:56 +0200

    Update Getset.java

--- src/Getset.java
+++ src/Getset.java
@@ -132,6 +132,18 @@ public class Getset {
             System.out.println("Something went wrong" + ex.getMessage());
         }
     }
+    public void resetSuspend(int userId){
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bibbla?serverTimezone=UTC",
+                "root","hammarby")) {
+            System.out.println("Connected");
+
+            PreparedStatement bookIn = conn.prepareStatement("UPDATE User SET suspendDate = null  WHERE User.id = " + "'" + userId + "'");
+            bookIn.executeUpdate();
+        }
+        catch (SQLException ex) {
+            System.out.println("Something went wrong" + ex.getMessage());
+        }
+    }
 
     public static void main(String[] args) {
         Getset hej = new Getset();
