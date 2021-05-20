b18dcd5170d1afc9f3b29d5a92a33fbfdcfe05d1
JoelJonson <joel.jonson@outlook.com>
2021-05-20 17:06:36 +0200

    Update AdminInteraction.java

--- src/AdminInteraction.java
+++ src/AdminInteraction.java
@@ -1,18 +1,76 @@
-import java.sql.Date;
+import java.util.Date;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 public class AdminInteraction {
+    Getset hoj;
 
-    public void addUser(User User){
-        //select statement som kollar ifall
-
+    AdminInteraction(Getset obj){
+        hoj = obj;
     }
 
-    public void deleteUser(){
+    /* public void deleteUser(int userId){
+         getSet = new Getset();
+         int nummer = object.getAUser(userId).getId();
+
+         if (nummer == userId) {
+             object.deleteUser(userId);
+         }
+         else {
+             System.out.println("User dosen't exist");
+         }
+     }
+ */
+    public boolean checkIfSuspended(int UserId) {
+        Getset hoj = new Getset();
+        HelpMethod object = new HelpMethod(hoj);
+
+        Date datum = object.getAUser(UserId).getSuspendDate();
+        System.out.println("Användarens datum: " + datum);
+        java.util.Date dagensdatum = java.sql.Date.valueOf(LocalDate.now());
+        System.out.println("Dagensdatum: " + dagensdatum);
+
+        if (datum == null){
+            System.out.println("Användaren är inte suspenderad!");
+            return false;
+        }
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(datum);
+        c.add(Calendar.DATE, 15);
+        if (c.getTime().compareTo(dagensdatum) < 0) {
+            object.resetSuspend(UserId);
+            System.out.println("Det har gått 15 dagar");
+            return true;
+        }
+
+        System.out.println("Är suspenderad");
+        return false;
     }
 
-    public void checkIfUserDelayed(int id){
-       
+    /*
+    public boolean suspendUser (int UserId){
+        object = new HelpMethod();
+        getSet = new Getset();
 
+        int antaldelays =0;
+        if (object.getAUser(UserId).Delays <=2 || object.getAUser(UserId).Delays ==4 || object.getAUser(UserId).Delays ==5 || object.getAUser(UserId).Delays ==7 || object.getAUser(UserId).Delays ==8 ){
+            return false;
+        }
+        else if (object.getAUser(UserId).Delays == 9){
+            return false;
+        }
+        if(object.getAUser(UserId).Delays >2){
+            object.getAUser(UserId).setSuspendDate(java.sql.Date.valueOf(LocalDate.now()));
+            getSet.suspendUser(UserId,java.sql.Date.valueOf(LocalDate.now()));
+            antaldelays = object.getAUser(UserId).Delays +1;
+            object.setDelays(UserId,antaldelays);
+            //getAUser(UserId).setDelays(+1);
+        }
+        return true;
+        //if a member delays to return library items more than twice,
+        // he/she gets suspended for 15 days.
+        // If he/she has been suspended more than twice, then the account is deleted.
     }
 
     public void checkIfBookIsAvaible(int ISBN)  { //inparametern blir en metod (requestForBook)
@@ -27,8 +85,47 @@ public class AdminInteraction {
         // and the system updates the number of the member’s borrowed items appropriately and the available items of this title.
     }
 
-    public void ReturnBook (int PersonID, int bookId){
-        //Kolla ifall usern finns, ta bort antal lånade items på usern
-        //Uppdatera i BookRegister att en bok av denna titel är tillgänglig
+    public void ReturnBook (int userId, int bookId) {
+        getSet = new Getset();
+        object = new HelpMethod();
+
+        int lanadeBocker;
+        int nummer = object.getAUser(userId).getId();
+
+        if (nummer == userId) {
+            lanadeBocker = object.getAUser(userId).getItemBorrowed();
+            lanadeBocker = lanadeBocker - 1;
+            getSet.removeBookFromUSer(bookId);
+            getSet.updateItemBorrowed(lanadeBocker, userId);
+        }
+
+        else {
+            System.out.println("User dosen't exist");
+        }
     }
+
+    public void RegisterUser(String Fnamn, String Lnamn, int PersonID, int Typ){
+        getSet = new Getset();
+        object = new HelpMethod();
+
+        for(User s: object.getUsers()){
+            if(s.getPersonId() == PersonID){
+                System.out.println("Användare är redan registrerad!");
+            }
+            if(s.getSuspendDate() != null){
+                System.out.println("Användare är suspenderad");
+            }
+        }
+        int Id = object.generateUserId();
+        getSet.setUser(Id, Fnamn, Lnamn, PersonID, Typ, 0, Typ, 1,0, null);
+    }
+
+*/
+
+    public static void main(String[] args) {
+        //hej.RegisterUser("Martin", "Nilssn", 880528, 3);
+
+    }
+
+
 }
