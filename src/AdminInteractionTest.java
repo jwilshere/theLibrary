a7213eef948d14e67e2d974c709746ad8e4e4b72
JoelJonson <joel.jonson@outlook.com>
2021-05-24 14:45:35 +0200

    h

--- src/AdminInteractionTest.java
+++ src/AdminInteractionTest.java
@@ -135,6 +135,7 @@ public class AdminInteractionTest {
         when(mock.getUsers())
                 .thenReturn(hej);
 
+
         for(User s: hej){
             System.out.println(s.getId());
         }
@@ -147,7 +148,44 @@ public class AdminInteractionTest {
         }
         System.out.println(hejda.getAUserOnPersonId(910403));
     }
+    @Test
+    void checkIfUserCanLend() throws SQLException{
+        Getset mock = mock(Getset.class);
+        AdminInteraction AI = new AdminInteraction(mock);
+        HelpMethod HM = new HelpMethod(mock);
 
+        //Skapar två "book"-objekt, samma objekt som förs i den förväntade arrayen som förs in i användarens lånade.
+        Book book1 = new Book(1, "Jogge", 22, null);
+        Book book2 = new Book(2, "Joggelito", 33, null);
+
+        //Skapar en array och lägger in de två "book"-objekten
+        Book[] expectedBooks = new Book[10];
+        expectedBooks[0] = book1;
+        expectedBooks[1] = book2;
+
+        //Skapar en Arraylist och lägger in en användare
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(4444, "jogel", "Jonson", 1111, 3, 0, 3, 1, 0, null));
+
+        //Lägger till de två "book"-objekten i en Arraylist
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+
+        //Mockar Användaren och Böckerna så att vi blir oberoende av databasen
+        when(mock.getUsers())
+                .thenReturn(users);
+
+        when(mock.getBooks())
+                .thenReturn(books);
+
+        AI.checkIfUserCanLend(22, 4444);
+        AI.checkIfUserCanLend(33, 4444);
+
+        //Testar så att vår förväntade array av användarens böcker är samma som den faktiska
+        assertArrayEquals(expectedBooks, HM.getAUser(4444).getBookLista());
+        System.out.println("Expected: " + Arrays.toString(expectedBooks) + "\nFaktiska: " + Arrays.toString(HM.getAUser(4444).getBookLista()));
+    }
 
 
 
